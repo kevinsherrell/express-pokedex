c3ba53ecc2b45c45f8e6229759cab31f442a8b41
kevinsherrell <k.sherrell.1028@gmail.com>
2021-02-22 08:20:53 -0700

    mvp complete..its ugly, but complete

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const pokemon = require('./models/pokemon.js');
 
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 
@@ -21,11 +23,16 @@ app.get('/new', (req, res) => {
     res.render('new');
 });
 // GET pokemon form to edit pokemon data
-app.get('/edit', (req, res) => {
-    console.log('Edit existing pokemon');
+app.get('/:id/edit', (req, res) => {
+    let selectedPokemon = pokemon.find(pkmn => {
+        return pkmn.id === req.params.id;
+    })
+
+    res.render('edit', {data: selectedPokemon});
 });
 // GET pokemon by id
 app.get('/:id', (req, res) => {
+    console.log('this route works');
     let selectedPokemon = pokemon.find(pkmn => {
         return pkmn.id === req.params.id;
     })
@@ -43,14 +50,35 @@ app.post('/', (req, res) => {
 });
 // PUT update pokemon data by id
 app.put("/:id", (req, res) => {
-    // pokemon[req.params.id]['name'] = req.body.name;
-    // pokemon[req.params.id]['type'] = req.body.type;
-    pokemon[req.params.id] = Object.assign(pokemon[req.params.id], req.body);
-    res.send(pokemon[req.params.id]);
+    console.log(req.body);
+    let selectedPokemon = pokemon.find(pkmn => {
+        return pkmn.id === req.params.id;
+    })
+    if (selectedPokemon) {
+        let updatedPokemon = Object.assign(selectedPokemon, req.body);
+        pokemon[selectedPokemon] = updatedPokemon;
+        res.redirect(`/${updatedPokemon.id}`)
+    } else {
+        res.send('pokemon does not exist');
+    }
+    ;
+
 });
 // DELETE delete pokemon by id
 app.delete('/:id', (req, res) => {
-    console.log('delete pokemon by id');
+    console.log(req.body);
+    let selectedPokemon = pokemon.find(pkmn => {
+        return pkmn.id === req.params.id;
+    })
+    if (selectedPokemon) {
+        const pokemonIndex = pokemon.indexOf(selectedPokemon)
+        pokemon.splice(pokemonIndex, 1);
+        res.render('index', {data: pokemon})
+    } else {
+        res.send('pokemon does not exist');
+    }
+    ;
+
 });
 
 
