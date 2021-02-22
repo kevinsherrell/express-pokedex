b691fb22189ecd24c031baa3316949f370975a4a
kevinsherrell <k.sherrell.1028@gmail.com>
2021-02-21 18:22:21 -0700

    created routes

--- server.js
+++ server.js
@@ -13,14 +13,34 @@ app.use(express.json());
 
 
 // GET all pokemon
+app.get('/pokemon', (req, res) => {
+    console.log('Get all pokemon');
+});
 // GET pokemon by id
+app.get('/pokemon/:id', (req, res) => {
+    console.log('Get pokemon by id');
+});
 // GET pokemon form to enter new pokemon data
+app.get('/pokemon/new', (req, res) => {
+    console.log('Create new pokemon');
+});
 // GET pokemon form to edit pokemon data
+app.get('/pokemon/edit', (req, res) => {
+    console.log('Edit existing pokemon');
+});
 // POST new pokemon data
+app.post('/pokemon', (req, res) => {
+    console.log('Post new pokemon');
+});
 // PUT update pokemon data by id
+app.put("/pokemon/:id", (req, res) => {
+    console.log('update pokemon by id');
+});
 // DELETE delete pokemon by id
+app.delete('/pokemon/:id', (req, res) => {
+    console.log('delete pokemon by id');
+});
 
-//test
 app.listen(port, () => {
     console.log(`app is listening on port: ${port}`);
 });
